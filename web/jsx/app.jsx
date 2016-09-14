eebdf1d48525e77a3a4ab2ffd3a6cd781b9de7c1
Luis Ramirez <luis.ramirez@smartts.com.pe>
2016-09-14 14:08:35 -0500

    Table from static data

--- web/jsx/app.jsx
+++ web/jsx/app.jsx
@@ -1,6 +1,40 @@
+var data = [ {
+   "when" : "2 minutes ago",
+   "who" : "Jill Dupre",
+   "description" : "Created new account"
+},
+{
+   "when" : "1 hour ago",
+   "who" : "Lose White",
+   "description" : "Added fist chapter"
+},
+{
+   "when" : "2 hours ago",
+   "who" : "Jordan Whash",
+   "description" : "Created new account"
+} ];
+
+var rows = data.map(function(row){
+    return <tr>
+    	<td>{row.when}</td>
+    	<td>{row.who}</td>
+    	<td>{row.description}</td>
+    </tr>;
+});
+
 var App = React.createClass({
     render: function(){
-        return <div>Hello React World! Again!</div>;
+        return <div>
+        	<h1>Recent changes</h1>
+            <table>
+              <thead>
+                <th>When</th>
+                <th>Who</th>
+                <th>Description</th>
+              </thead>
+              {rows}
+            </table>
+        </div>;
     }
 });
 ReactDOM.render(React.createElement(App), document.body);
